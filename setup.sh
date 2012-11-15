a2edeba86aa487de81f0b65d2ff62bad3dd9ae94
YasuOza <yasuharu.ozaki@gmail.com>
2012-11-15 11:49:37 +0900

    git config --global core.excludesfile = ~/.gitignore_global

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DOT_FILES=( .zsh .zshrc .zshrc.alias .zshrc.linux .zshrc.osx .ctags   \
-            .emacs.el .gdbinit .gemrc .gitconfig .gitignore .inputrc  \
+            .emacs.el .gdbinit .gemrc .gitconfig .gitignore_global .inputrc  \
             .irbrc .sbtconfig .screenrc .vimrc .vrapperrc .tmux.conf  \
             .dir_colors .rdebugrc .perltidyrc .proverc .xvimrc .tigrc \
             .my.cnf .caprc .rspec .zshenv .zprofile )

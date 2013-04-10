b5359d4aec536e4b4a1286e0ecf2dc63a6501df9
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2013-04-10 11:03:07 +0900

    Add .jrubyrc to dotfiles

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ DOT_FILES=( .zsh .zshrc .zshrc.alias .zshrc.linux .zshrc.osx .ctags   \
             .emacs.el .gdbinit .gemrc .gitconfig .gitignore_global .inputrc  \
             .irbrc .sbtconfig .screenrc .vimrc .vrapperrc .tmux.conf  \
             .dir_colors .rdebugrc .perltidyrc .proverc .xvimrc .tigrc \
-            .my.cnf .caprc .rspec .zshenv .zprofile .hgrc )
+            .my.cnf .caprc .rspec .zshenv .zprofile .hgrc .jrubyrc )
 
 for file in ${DOT_FILES[@]}
 do

2ffb2aaec646ab10d74540bbf0cf1511d505da7d
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2014-02-18 15:37:14 +0900

    Migrating from zsh

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@
 # rbenv
 #=============================
 if test -d $HOME/.rbenv
-  set PATH $HOME/.rbenv/bin $PATH
+  set PATH $HOME/.rbenv/bin   $PATH
   set PATH $HOME/.rbenv/shims $PATH
   rbenv rehash >/dev/null ^&1
 end
@@ -15,17 +15,44 @@ if test -d $HOME/.nodebrew
 end
 
 #=============================
+# Go
+#=============================
+if test -d $HOME/.go
+  set GOPATH $HOME/.go
+end
+
+#=============================
+# plenv
+#=============================
+if test -d $HOME/.plenv
+  set PATH $HOME/.plenv/bin   $PATH
+  set PATH $HOME/.plenv/shims $PATH
+  rbenv plenv init - >/dev/null ^&1
+end
+
+#=============================
+# bin
+#=============================
+if test -d $HOME/.bin
+  set PATH $HOME/.bin $PATH
+end
+
+#=============================
 # plugins
 #=============================
 . $HOME/dotfiles/fish/plugins/z-fish/z.fish
+. $HOME/dotfiles/fish/plugins/extract/extract.fish
 
 #=============================
 # alias
 #=============================
-function t   ; command tig $argv        ; end
-function l   ; command ls $argv         ; end
-function git ; command hub $argv        ; end
-function gst ; git status $argv         ; end
-function g   ; git $argv                ; end
-function j   ; z $argv                  ; end
-function rmrf; command rm -rf $argv     ; end
+function t    ; command tig $argv         ; end
+function l    ; command ls $argv          ; end
+function git  ; command hub $argv         ; end
+function gst  ; git status $argv          ; end
+function g    ; git $argv                 ; end
+function j    ; z $argv                   ; end
+function rmrf ; command rm -rf $argv      ; end
+function ps?  ; command pgrep -l -f $argv ; end
+function du   ; command du -h  $argv      ; end
+function tm   ; command tmux  $argv       ; end

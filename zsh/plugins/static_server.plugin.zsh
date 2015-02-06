be47a4d5928b079f99f4be3511820200438752b7
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2015-02-06 10:13:25 +0900

    local static server port 8080

--- zsh/plugins/static_server.plugin.zsh
+++ zsh/plugins/static_server.plugin.zsh
@@ -2,8 +2,8 @@
 # http://blog.kamipo.net/entry/2013/02/20/122225
 function static_httpd {
   if which ruby > /dev/null; then
-    ruby -rwebrick -e 'WEBrick::HTTPServer.new(:Port => 5000, :DocumentRoot => ".").start'
+    ruby -rwebrick -e 'WEBrick::HTTPServer.new(:Port => 8080, :DocumentRoot => ".").start'
   elif which php > /dev/null && php -v | grep -qm1 'PHP 5\.[45]\.'; then
-    php -S 0.0.0.0:5000
+    php -S 0.0.0.0:8080
   fi
 }

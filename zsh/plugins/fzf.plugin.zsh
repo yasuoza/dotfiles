44777565a7a710ff8236db3f75c11a94a0a99eb4
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2016-11-04 17:55:44 +0900

    ^G^J starts selecting a git branch

--- zsh/plugins/fzf.plugin.zsh
+++ zsh/plugins/fzf.plugin.zsh
@@ -6,15 +6,15 @@ if type fzf &> /dev/null; then
     # Override default fzf option
     export FZF_DEFAULT_OPTS="--extended --cycle --reverse --ansi"
 
-    function fzf_select_history() {
+    function _fzf_select_history() {
         BUFFER=$(fc -l -n 1 | fzf-tmux --tac --ansi)
         CURSOR=$#BUFFER         # move cursor
         zle -R -c               # refresh
     }
-    zle -N fzf_select_history
-    bindkey '^R' fzf_select_history
+    zle -N _fzf_select_history
+    bindkey '^R' _fzf_select_history
 
-    function fzf_select_directory() {
+    function _fzf_select_directory() {
         # Remove score and space
         # '5.13      /PATH/TO/DESTINATION' => '/PATH/TO/DESTINATION'
         local dest="$(_z -r 2>&1 | fzf-tmux --tac | sed 's/^[0-9. ]*//g')"
@@ -24,12 +24,18 @@ if type fzf &> /dev/null; then
         fi
         zle reset-prompt
     }
-    zle -N fzf_select_directory
-    bindkey '^X^J' fzf_select_directory
+    zle -N _fzf_select_directory
+    bindkey '^X^J' _fzf_select_directory
 
-    # List all local branches
-    alias -g B='`command git branch | fzf-tmux | sed -e "s/^\*[ ]*//g"`'
-
-    # List all tmux sessions
-    alias -g S='`tmux ls | fzf-tmux | cut -d':' -f 1`'
+    # Select local git branche
+    function _fzf_select_local_git_branches() {
+        local branche=$(command git branch | fzf-tmux | sed -e "s/^\*[ ]*//g")
+        if [ -n $branche ]; then
+            BUFFER="$LBUFFER'${branche}'"
+            CURSOR=$#BUFFER         # move cursor
+            zle -R -c               # refresh
+        fi
+    }
+    zle -N _fzf_select_local_git_branches
+    bindkey '^G^J' _fzf_select_local_git_branches
 fi

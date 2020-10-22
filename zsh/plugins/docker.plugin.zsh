5503fb1b617a5861223d1a129b77aa46d584c949
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2020-10-23 08:20:43 +0900

    Support multiple docker image remove

--- zsh/plugins/docker.plugin.zsh
+++ zsh/plugins/docker.plugin.zsh
@@ -1,8 +1,8 @@
 #!zsh
 
 function docker-rmi() {
-  local image="$(docker images | sed -e '1d' | fzf-tmux --reverse | awk '{print $3}')"
-  if [ -n "${image}" ]; then
-    print -z "docker rmi -f ${image}"
+  local images="$(docker images | sed -e '1d' | fzf-tmux --multi --reverse | awk '{print $3}')"
+  if [ -n "${images}" ]; then
+    print -z "docker rmi -f $(echo $images | tr '\n' ' ')"
   fi
 }

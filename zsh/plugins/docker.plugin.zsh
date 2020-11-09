e62f78a7f83d63bf6931acd4ddeeb8e7fff77a90
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2020-11-09 22:28:46 +0900

    Add d-exec command

--- zsh/plugins/docker.plugin.zsh
+++ zsh/plugins/docker.plugin.zsh
@@ -6,3 +6,13 @@ function docker-rmi() {
     print -z "docker rmi -f $(echo $images | tr '\n' ' ')"
   fi
 }
+alias d-rmi=docker-rmi
+
+
+function docker-exec() {
+  local container="$(docker ps | sed -e '1d' | fzf-tmux --reverse | awk '{print $1}')"
+  if [ -n "${container}" ]; then
+    print -z "docker exec -it $container "
+  fi
+}
+alias d-exec=docker-exec

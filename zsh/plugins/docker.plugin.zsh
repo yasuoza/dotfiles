2aa4f7193728f860b6c0114fb409fd9b368c2007
Yasuharu Ozaki <yasuoza@users.noreply.github.com>
2026-01-26 17:32:36 +0900

    Docker.app updates

--- zsh/plugins/docker.plugin.zsh
+++ zsh/plugins/docker.plugin.zsh
@@ -1,7 +1,7 @@
 #!zsh
 
 function docker-rmi() {
-  local images="$(docker images | sed -e '1d' | fzf-tmux --multi --reverse | awk '{print $3}')"
+  local images="$(docker images | sed -e '1d' | fzf-tmux --multi --reverse | awk '{print $2}')"
   if [ -n "${images}" ]; then
     print -z "docker rmi -f $(echo $images | tr '\n' ' ')"
   fi
